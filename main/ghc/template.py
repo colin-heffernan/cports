37f4a0e53cbc9ad8397e6550096a499275c2b286
Colin Heffernan <colinpheffernan@gmail.com>
2026-06-16 10:02:03 -0400

    main/ghc: new package

---
+++ main/ghc/template.py
@@ -0,0 +1,21 @@
+pkgname = "ghc"
+pkgver = "9.8.4"
+pkgrel = 0
+hostmakedepends = ["ghc-devel", "python", "gtar", "xz", "automake"]
+pkgdesc = "Glasgow Haskell Compiler"
+license = "BSD-3-Clause"
+url = "https://www.haskell.org/ghc"
+source = f"https://downloads.haskell.org/ghc/{pkgver}/ghc-{pkgver}-x86_64-alpine3_12-linux-static.tar.xz"
+sha256 = "02535eaef4f57aef8b7b272ef6f48d386d186ebd07fa47e227b46b93fa7eb0ab"
+
+
+def configure(self):
+    self.do("./configure", "--prefix=/usr")
+
+
+def install(self):
+    self.do("make", "install", f"DESTDIR=../..{self.chroot_destdir}")
+
+
+def post_install(self):
+    self.install_license("share/doc/x86_64-linux-ghc-9.8.4/ghc-9.8.4/LICENSE")
